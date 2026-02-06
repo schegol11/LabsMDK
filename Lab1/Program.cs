4188a372967fc1418730e7062cb2a3d3737ce07f
Schegol11 <69989869+schegol11@users.noreply.github.com>
2026-02-06 12:23:44 +0300

    Create Program.cs

---
+++ Lab1/Program.cs
@@ -0,0 +1,52 @@
+//Задание 74
+
+Decimal dec = new();
+
+Console.WriteLine(dec.Compute());
+dec.Print();
+
+Binary bin = new();
+
+Console.WriteLine(bin.Compute());
+bin.Print();
+
+abstract Integer
+{
+
+    public int Compute();
+
+    public void Print();
+}
+
+public class Decimal : Integer
+{
+    public decimal[] decimalArray = { 1,1, 2,2, 3,3};
+
+    public int Compute()
+    {
+        int decResult = 0;
+        foreach (var dec in decimalArray) decResult += (int)dec;
+
+        return decResult;
+    }
+
+    public void Print() => Console.WriteLine("Курага");
+}
+
+public class Binary : Integer
+{
+    public int[] binaryArray = { 1, 0, 1, 0, 1 };
+
+    public int Compute()
+    {
+        int binResult = 0;
+        foreach (var bin in binaryArray)
+        {
+            binResult *= 10;
+            if (bin == 1) binResult += 1;
+        }
+        return binResult;
+    }
+
+    public void Print() => Console.WriteLine("Егор");
+}
